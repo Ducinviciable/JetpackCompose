fbd7ff2355061a63055fb7d014b0cb2cf45a215f
Ducinviciable <ductelecompro@gmail.com>
2025-04-26 22:45:30 +0700

    first commit

---
+++ Course1/app/src/main/java/com/example/myapplication/ui/BasicState.kt
@@ -0,0 +1,77 @@
+package com.example.myapplication.ui//package com.example.myapplication//package com.example.ktgk.ui.Basic
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.animation.core.animateDpAsState
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.ElevatedButton
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//
+//
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//
+//class BasicState: ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//                MyApp(modifier = Modifier.fillMaxSize())
+//        }
+//    }
+//}
+//
+//@Composable
+//fun MyApp(
+//    modifier: Modifier = Modifier,
+//    names: List<String> = listOf("World", "Compose")
+//) {
+//    Spacer( modifier = Modifier.height(20.dp))
+//    Column(modifier = modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name = name)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    val expanded = remember { mutableStateOf(false) }
+//    val extraPadding = if (expanded.value) 48.dp else 0.dp
+//    Surface(
+//        color = MaterialTheme.colorScheme.primary,
+//        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+//    ) {
+//        Row(modifier = Modifier.padding(24.dp)) {
+//            Column(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(bottom = extraPadding)
+//            ) {
+//                Text(text = "Hello ")
+//                Text(text = name)
+//            }
+//            ElevatedButton(
+//                onClick = { expanded.value = !expanded.value }
+//            ) {
+//                Text(if (expanded.value) "Show less" else "Show more")
+//            }
+//        }
+//    }
+//}
+//
